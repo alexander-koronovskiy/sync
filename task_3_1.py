fd9f0573bb7a4912e0a7bd4d08b003be35c4ff8b
alexander <alexander.koronovskiy@gmail.com>
2019-03-11 19:48:35 +0400

    with the right names of variables

--- task_3_1.py
+++ task_3_1.py
@@ -5,15 +5,15 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # lorenz_f params
 sigma = 10.0
-rho = 28.0
-beta = 8.0 / 3.0
+r_d = 28.0
+r_r = 8.0 / 3.0
 
 
 def lorenz_f(state, t):
   x, y, z = state           # unpack the state vector
   return sigma * (y - x),   \
-         x * (rho - z) - y, \
-         x * y - beta * z   # derivatives
+         x * (r_d - z) - y, \
+         x * y - r_r * z    # derivatives
 
 
 def plot_f(x, y, z):
