57f004c55ed386ad403befb68c693d32e36c2d1d
alexander <alexander.koronovskiy@gmail.com>
2019-03-30 02:20:52 +0400

    my tasks only

--- task_3_1.py
+++ task_3_1.py
@@ -1,43 +1,41 @@
+# imports
+import sympy
+from scipy.integrate import odeint
+import re as regex
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import odeint
-from mpl_toolkits.mplot3d import Axes3D
 
-# lorenz_f params
-sigma = 10.0
-r_d = 40
-b = 8.0 / 3.0
-r_r = 35.0
-eps = 0.00001
+# define all symbols I am going to use
+x = sympy.Symbol('x')
+y = sympy.Symbol('y')
+t = sympy.Symbol('t')
 
-
-def lorenz_f(state, t):
-  x_d, y_d, z_d = state  # unpack the state vector
-  return sigma * (y_d - x_d), \
-         x_d * (r_d - z_d) - y_d, \
-         x_d * y_d - b * z_d
+# read the file
+systemOfEquations = []
+with open("system.txt", "r") as fp:
+   for line in fp:
+            pattern = regex.compile(r'.+?\s+=\s+(.+?)$')
+            expressionString = regex.search(pattern, line)
+            # first match ends in group(1)
+            systemOfEquations.append(sympy.sympify(expressionString.group(1)))
 
 
-def lorenz_f_r(state, t):
-    x_r, y_r, z_r, x_d, y_d, z_d = state
-    return sigma * (y_d - x_d), \
-         x_d * (r_d - z_d) - y_d, \
-         x_d * y_d - b * z_d, \
-         sigma * (y_r - x_r) + eps * (x_d - x_r), \
-         r_r * x_r - y_r - x_r * z_r, \
-         x_r * y_r - b * z_r
+def dX_dt(X, t):
+    vals = dict(x=X[0], y=X[1], t=t)
+    return [eq.evalf(subs=vals) for eq in systemOfEquations]
 
 
-def plot_f(x_d, y_d, z_d):
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.plot(x_d, y_d, z_d)
-    plt.show()
+def do_solver_result(arg_num):
+    solver_result = []
+    solver_array = odeint(dX_dt, [1, 2], np.linspace(0, 100, 20))
+    for i in range(len(solver_array)):
+        solver_result.append(solver_array[i][arg_num])
+    return solver_result
 
 
 if __name__ == '__main__':
-    state0 = [1.0, 1.0, 1.0]
-    t = np.arange(0.0, 40.0, 0.01)
-    states = odeint(lorenz_f, state0, t)
-    print(states)
-    plot_f(states[:, 0], states[:, 1], states[:, 2])
+    plt.plot(do_solver_result(0), do_solver_result(1))
+    plt.title('time realisation')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
