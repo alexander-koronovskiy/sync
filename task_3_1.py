dca6acf8553b9b80837b6c684cfe947106f5bdf6
alexander <alexander.koronovskiy@gmail.com>
2019-03-30 04:57:18 +0400

    very long working programm

--- task_3_1.py
+++ task_3_1.py
@@ -4,6 +4,7 @@ from scipy.integrate import odeint
 import re as regex
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # define all symbols I am going to use
 x = sympy.Symbol('x')
@@ -26,17 +27,20 @@ def dX_dt(X, t):
     return [eq.evalf(subs=vals) for eq in systemOfEquations]
 
 
-def do_solver_result(arg_num):
+def do_s_result(arg_num):
     solver_result = []
-    solver_array = odeint(dX_dt, [0.1, 0.1, 0.1], np.linspace(0, 10, 20))
+    solver_array = odeint(dX_dt, [0.1, 0.1, 0.1], np.linspace(0, 10, 500))
     for i in range(len(solver_array)):
         solver_result.append(solver_array[i][arg_num])
     return solver_result
 
 
-if __name__ == '__main__':
-    plt.plot(do_solver_result(0), do_solver_result(2))
-    plt.title('time realisation')
-    plt.xlabel('x')
-    plt.ylabel('z')
+def plot_f(x, y, z):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot(x, y, z)
     plt.show()
+
+
+if __name__ == '__main__':
+    plot_f(do_s_result(0), do_s_result(1), do_s_result(2))
