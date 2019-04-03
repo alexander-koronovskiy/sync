6555f98ed75b35b7b32a50e44e83ea0d742416d7
alexander <alexander.koronovskiy@gmail.com>
2019-04-03 13:27:54 +0400

    upgrage main task

--- task_3_1.py
+++ task_3_1.py
@@ -36,12 +36,8 @@ def dX_dt(X, t):
     return [eq.evalf(subs=vals) for eq in systemOfEquations]
 
 
-def do_s_result(arg_num):
-    solver_result = []
-    solver_array = odeint(dX_dt, [1, 1, 0, 1, 1, 0], np.linspace(0, 10, 500))
-    for i in range(len(solver_array)):
-        solver_result.append(solver_array[i][arg_num])
-    return solver_result
+def do_s_result():
+    return odeint(dX_dt, [1, 1, 0, 1, 1, 0], np.linspace(0, 20, 400))
 
 
 def plot_f(x, y, z):
@@ -59,10 +55,31 @@ def do_phase_shift(X, Y):
     return phase_array
 
 
+def do_diff_arrays(x, y):
+    a = [x]
+    b = [y]
+    c = [list(map(lambda a, b: a - b, a[i], b[i])) for i in range(len(a))]
+    return c[0]
+
+
+def euler_method(state, increment, dt):
+    new_state = list(map(lambda x: x[0] + x[1] * dt, zip(state, increment)))
+    return new_state
+
+
 def save_s_result():
-    var_names = [x, y, z, u, v, w]
-    for i in range(6):
-        WorkWFiles.write_to_file(do_s_result(i), 'solutions_' + relation_type + '/' + str(var_names[i]) + '.txt')
+    arr = do_s_result()
+    x_arr = []; y_arr = []; z_arr = []; u_arr = []; v_arr = []; w_arr = []
+    for i in range(len(arr)):
+        x_arr.append(arr[i][0]); y_arr.append(arr[i][1]); z_arr.append(arr[i][2])
+        u_arr.append(arr[i][3]); v_arr.append(arr[i][4]); w_arr.append(arr[i][5])
+
+    WorkWFiles.write_to_file(x_arr, 'solutions_' + relation_type + '/x.txt')
+    WorkWFiles.write_to_file(y_arr, 'solutions_' + relation_type + '/y.txt')
+    WorkWFiles.write_to_file(z_arr, 'solutions_' + relation_type + '/z.txt')
+    WorkWFiles.write_to_file(u_arr, 'solutions_' + relation_type + '/u.txt')
+    WorkWFiles.write_to_file(v_arr, 'solutions_' + relation_type + '/v.txt')
+    WorkWFiles.write_to_file(w_arr, 'solutions_' + relation_type + '/w.txt')
 
 
 if __name__ == '__main__':
@@ -81,8 +98,8 @@ if __name__ == '__main__':
     plot_f(x_sol, y_sol, z_sol)
     plot_f(u_sol, v_sol, w_sol)
     # dependence between phase and time
-    a = do_phase_shift(x_sol, u_sol)
-    plt.title(relation_type + ' relation type')  # weak: ε = 0.01; strong: ε = 10
+    a = do_diff_arrays(do_phase_shift(x_sol, y_sol), do_phase_shift(u_sol, v_sol))
+    plt.title(relation_type + ' relation type')  # weak: ε = 0.1; strong: ε = 10
     plt.xlabel('Time[t]')
     plt.ylabel('Phase[$f$]')
     plt.plot(a)
